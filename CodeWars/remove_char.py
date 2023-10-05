1dd4f49761156fd30ff4d95489d417ce3833aed6
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-10-05 10:47:37 -0600

    remove_char.py start

---
+++ CodeWars/remove_char.py
@@ -0,0 +1,24 @@
+
+"""
+It's pretty straightforward. Your goal is to create a function that
+removes the first and last characters of a string. You're given one
+parameter, the original string. You don't have to worry with strings
+with less than two characters.
+"""
+
+# simulating input to test it locally
+def main():
+    user_input = input("User input: ")
+    function_return = find_smallest_int(user_input)
+    print(function_return)
+
+
+def remove_char(s):
+    #your code here
+
+
+
+
+
+if __name__ == "__main__":
+    main()
