d1e800cd574744ba7717d978934eb20763c30451
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-10-11 17:20:36 -0600

    first commit for repeat_str.py

---
+++ find_smallest_int.py
@@ -0,0 +1,31 @@
+
+"""
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+Given [34, 15, 88, 2] your solution will return 2
+Given [34, -345, -1, 100] your solution will return -345
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+"""
+
+# simulating input to test it locally
+def main():
+    user_input = input("User input: ")
+    function_return = find_smallest_int(user_input)
+    print(function_return)
+
+
+def find_smallest_int(arr):
+    smallest_number = 9999
+    for number in arr:
+        if number < smallest_number:
+            smallest_number = number
+    return smallest_number
+
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ remove_char.py
@@ -0,0 +1,22 @@
+
+"""
+It's pretty straightforward. Your goal is to create a function that
+removes the first and last characters of a string. You're given one
+parameter, the original string. You don't have to worry with strings
+with less than two characters.
+"""
+
+# simulating input to test it locally
+def main():
+    user_input = input("User input: ")
+    function_return = remove_char(user_input)
+    print(function_return)
+
+
+def remove_char(s):
+    return s[1:-1]
+
+
+
+if __name__ == "__main__":
+    main()
