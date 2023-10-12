77ce6bef18a8b1d0e3ef34f77229535742895a58
Gytenis Borusas <gytenis.borusas@gmail.com>
2023-10-12 13:09:16 -0600

    mid rgb.py file

--- rgb.py
+++ rgb.py
@@ -1,6 +1,7 @@
 """
 
-The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
+The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal 
+representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
 
 Note: Your answer should always be 6 characters long, the shorthand with 3 will not work here.
 
@@ -12,8 +13,62 @@ Examples (input --> output):
 
 """
 
+# simulating input to test it locally
+def main():
+    user_input = input("User input: ")
+    ui1, ui2, ui3 = user_input.split(",")
+    function_return = rgb(ui1, ui2, ui3)
+    print(function_return)
+
+
 
 def rgb(r, g, b):
-    # your code here :)
-    pass
+    
+    r = int(r)
+    g = int(g)
+    b = int(b)
 
+    #checks if "r" is within the range of 0 to 255
+    if r > 255:
+        r = 255
+    if r < 0:
+        r = 0
+    else:
+        r = r
+
+    #checks if "g" is within the range of 0 to 255
+    if g > 255:
+        g = 255
+    if g < 0:
+        g = 0
+    else:
+        g = g
+
+    #checks if "b" is within the range of 0 to 255
+    if b > 255:
+        b = 255
+    if b < 0:
+        b = 0
+    else:
+        b = b
+
+    hex_r = "{:02X}".format(r)
+    hex_g = "{:02X}".format(g)
+    hex_b = "{:02X}".format(b)
+
+    print(hex_r)
+    print(hex_g)
+    print(hex_b)
+
+    hexadecimal = hex_r + hex_g + hex_b
+    hexadecimal = hexadecimal.upper()
+    print(hexadecimal)
+
+    return hexadecimal
+
+
+
+
+
+if __name__ == "__main__":
+    main()
